fecf4cc70faf657e6455ca723ad3dace0dedeb44
RISkylollipop <kelanikdas1@gmail.com>
2023-10-05 22:22:06 +0000

    adding printf to code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
