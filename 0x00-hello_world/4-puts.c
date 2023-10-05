8eb69cb429d967898d6c11c09d724c276355afb2
RISkylollipop <kelanikdas1@gmail.com>
2023-10-05 22:00:51 +0000

    the initial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
+/**
+ * main - program that prints a sentence
+ * Return: 0
+ */
 #include <stdio.h>
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
