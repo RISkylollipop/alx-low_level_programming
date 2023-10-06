3114907d5c5dc291cd11b8859bd564f358cd5ad4
RISkylollipop <kelanikdas1@gmail.com>
2023-10-05 18:34:53 -0700

    debugging size of file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long int J;
 	float K;
 
-	printf("size of a char: %zu byte(s)\n", sizeof(F));
-	printf("size of a int: %zu byte(s)\n", sizeof(G));
-	printf("size of a long int: %zu byte(s)\n", sizeof(H));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(J));
-	printf("size of a float: %zu byte(s)\n", sizeof(K));
-		return(0);
+	printf("size of a char: %lu byte(s)\n", sizeof(F));
+	printf("size of a int: %lu byte(s)\n", sizeof(G));
+	printf("size of a long int: %lu byte(s)\n", sizeof(H));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(J));
+	printf("size of a float: %lu byte(s)\n", sizeof(K));
+	return (0);
 }
