88fc8fa2fc1ea3421f8cfc76ef44c51e45724b3c
RISkylollipop <kelanikdas1@gmail.com>
2023-10-05 18:39:16 -0700

    success debug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long int J;
 	float K;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(F));
-	printf("size of a int: %lu byte(s)\n", sizeof(G));
-	printf("size of a long int: %lu byte(s)\n", sizeof(H));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(J));
-	printf("size of a float: %lu byte(s)\n", sizeof(K));
+	printf("size of a char: %d byte(s)\n", sizeof(F));
+	printf("size of a int: %d byte(s)\n", sizeof(G));
+	printf("size of a long int: %d byte(s)\n", sizeof(H));
+	printf("size of a long long int: %d byte(s)\n", sizeof(J));
+	printf("size of a float: %d byte(s)\n", sizeof(K));
 	return (0);
 }
