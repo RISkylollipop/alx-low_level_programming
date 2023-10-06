b19146a3708696e0c85aab3827ee41c1974761fd
RISkylollipop <kelanikdas1@gmail.com>
2023-10-05 18:07:34 -0700

    with character long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,7 @@ int main(void)
 	printf("size of a int: %zu byte(s)\n", sizeof(G));
 	printf("size of a long int: %zu byte(s)\n", sizeof(H));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(J));
-	printf("size of a float: %zu byte(s)\n", sizeof(K));
+	printf("size of a float: %zu byte(s)\n", sizeof(K)),
+
 	return (0);
 }
