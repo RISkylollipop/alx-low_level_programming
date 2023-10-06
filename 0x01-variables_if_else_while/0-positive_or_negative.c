9697e9bd5fb738e5779719e291855338f01dc4a5
RISkylollipop <kelanikdas1@gmail.com>
2023-10-06 16:36:12 -0700

    commitment pays off

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	B = n % 10;
-	if (B > 5)
+	B = n % 15;
+	if (B < 5)
 	{
-		printf("Last digit of %d is negative\n", n, B);
+		printf("Last digit of %d is %d and is less than 5\n", n, B);
 	}
 	else if (B == 0)
 	{
-		printf("Last digit of %d is zero\n", n, B);
+		printf("Last digit of %d is %d and is 0\n", n, B);
 	}
 	else
 	{
-		printf("Last digit of %d is  positive\n", n, B);
+		printf("Last digit of %d is %d and is greater than 6 and not 0\n", n, B);
 	}
 	return (0);
 }
