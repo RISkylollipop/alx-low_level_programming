444e009712cdab0cbb87f02857b96306f64baab8
RISkylollipop <kelanikdas1@gmail.com>
2023-10-10 10:49:12 -0700

    printing of alphabeth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - To print alphabet with new line
+ * print_alphabet - To print complete alphabet
+ * Return: (0)
+ */
+
+	void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar('x');
+	}
+
+	_putchar('\n');
+}
