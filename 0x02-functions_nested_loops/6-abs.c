7a8841586dad8a6d51188589b2d6be5a79dbda59
RISkylollipop <kelanikdas1@gmail.com>
2023-10-10 17:24:48 -0700

    tough debug

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,21 @@
 /**
  * _abs - TO print absolute number of an nteger
  * @n: The compute number
- * Return; (0)
+ * Return: (0)
  */
+
 int _abs(int n)
 
 {
 
 	if (n < 0)
+
 	{
-		int abs_value;
-		abs_value = n * -1;
-		return (abs_value);
+	int abs_value;
+
+	abs_value = n * -1;
+
+	return (abs_value);
 	}
 	return (n);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int n)
 	lst = n % 10;
 	if (lst < 0)
 	{
-		lst = lst * -1;
+	lst = lst * -1;
 	}
 	_putchar(lst + '0');
 	return (lst);
